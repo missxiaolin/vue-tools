cf7726bb8a5db1babc0bf9fc06d3b62bd703851c
lingongbing <462441355@qq.com>
2022-11-10 19:49:54 +0800

    * add

--- src/main.js
+++ src/main.js
@@ -5,11 +5,14 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import { i18n } from './locales'; // 引入 src/i18n/index，得到 i18n 实例
+
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   router,
   store,
   render: h => h(App)
