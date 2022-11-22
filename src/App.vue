d8965ad7570b141ffdf6807d57fef833e7ef44d6
lingongbing <462441355@qq.com>
2022-11-22 14:48:03 +0800

    * 路由模块

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,53 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" class="theme_red">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/shopping">购物车</router-link>|
+    </div>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  name: "app",
+  mounted() {
+    this.$nextTick(() => {
+      let link = document.createElement("link");
+      link.type = "text/css";
+      link.id = "theme";
+      link.rel = "stylesheet";
+      link.href = "./theme_red.css";
+      console.log(link);
+      document.getElementsByTagName("head")[0].appendChild(link);
 
+      // document.getElementById('app').setAttribute('class', 'theme_red');
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+:root {
+  --theme_bg_color: #eb6161;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
