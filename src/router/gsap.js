7cf653a1346d4e0aa083aee897293df8a64a1246
lingongbing <462441355@qq.com>
2024-04-08 15:18:51 +0800

    * canvas 逐帧动画

--- src/router/gsap.js
+++ src/router/gsap.js
@@ -2,21 +2,17 @@ export default [
   {
     path: "/gsap",
     name: "gsap",
-    component: () =>
-      import(/* webpackChunkName: "gsap" */ "../views/gsap/index.vue"),
+    component: () => import("../views/gsap/index.vue"),
   }, {
     path: "/gsap/scale/phone/scroll",
     name: "gsapScalePhoneScroll",
-    component: () =>
-      import(
-        /* webpackChunkName: "gsapScalePhoneScroll" */ "../views/gsap/vivo-scroll-switch-phone-color/scale-phone-to-middle-when-scroll.vue"
-      ),
+    component: () => import("../views/gsap/vivo-scroll-switch-phone-color/scale-phone-to-middle-when-scroll.vue"),
   }, {
     path: "/gsap/swich/phone/scroll",
     name: "gsapScalePhoneScroll",
-    component: () =>
-      import(
-        /* webpackChunkName: "gsapScalePhoneScroll" */ "../views/gsap/vivo-scroll-switch-phone-color/switch-phone-color-when-scroll.vue"
-      ),
-  }
+    component: () => import("../views/gsap/vivo-scroll-switch-phone-color/switch-phone-color-when-scroll.vue"),
+  }, {
+    path: "/gsap/canvas/animation",
+    name: "canvasAnimation",
+    component: () => import("../views/gsap/macbook-pro-animation/canvas-animation.vue"),}
 ];
--- src/views/gsap/index.vue
+++ src/views/gsap/index.vue
@@ -5,6 +5,10 @@
         <router-link to="/gsap/scale/phone/scroll">scale-phone-to-middle-when-scroll</router-link> | 
         <router-link to="/gsap/swich/phone/scroll">switch-phone-color-when-scroll</router-link> 
     </nav>
+    <p>MacBook Pro 新品发布官网动画效果实现(2023年01月)</p>
+    <nav>
+      <router-link to="/gsap/canvas/animation">switch-phone-color-when-scroll</router-link> 
+    </nav>
   </div>
 </template>
 
